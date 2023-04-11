373e4e354794eadc5e72070846a8d2fae8266455
Samvit Swaminathan <samvit.swaminathan@addverb.com>
2023-04-11 17:55:34 +0530

    controller, model and views folders made

--- main.go
+++ main.go
@@ -4,16 +4,16 @@ import (
 	"encoding/json"
 	"net/http"
 
-	// "github.com/Samvit20/Todo-API-using-Golang/tree/master/structs"
+	"github.com/Samvit20/Todo-API-using-Golang/views"
 )
 
 func main() {
 	mux := http.NewServeMux()	
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method ==  http.MethodGet {
-			data := structs.Response {
+			data := views.Response {
 				Code: http.StatusOK,
-				Body: "pong",
+				Body: "ping",
 			}
 			json.NewEncoder(w).Encode(data)
 		}
